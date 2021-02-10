1f78817b81fed0e522dc41ea1fe321427ff5fd37
Dan Pappo <dpappo@gmail.com>
2021-02-09 20:06:59 -0500

    completed page downloader challenge

---
+++ fetcher.js
@@ -0,0 +1,23 @@
+const request = require("request");
+const fs = require("fs");
+
+const myArgs = process.argv.slice(2);
+
+const URL = myArgs[0];
+const path = myArgs[1];
+
+const fetcher = function (URL, path) {
+  request(`${URL}/${path}`, (error, response, body) => {
+    console.log("error:", error); // Print the error if one occurred
+    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+    let data = body;
+    // console.log("body:", body); // Print the HTML for the Google homepage.
+    fs.writeFile(path, data, () => {
+      console.log(
+        `Downloaded and saved ${fs.statSync(path).size} bytes to ${path}`
+      );
+    });
+  });
+};
+
+fetcher(URL, path);
